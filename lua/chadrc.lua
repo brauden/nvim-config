f5f3258b862f704b2ac7dffcc53bec3e3e543e80
Dauren Bizhanov <dauren.bizhanov@duke.edu>
2025-11-15 23:31:53 -0500

    add: color changes to onedark theme

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,19 +5,27 @@ M.base46 = {
   changed_themes = {
     onedark = {
       base_16 = {
-        base0B = "#60778a", -- strings
-        base0C = "#dbc202",
-        base0A = "#dbc202", -- classes
-        base08 = "#c8ccd4",
         base00 = "#282936",
-        base0D = "#C678DD", -- purple for functions/methods
-        base0E = "#D19A66", -- keywords
+        base0B = "#60778a", -- strings
+        base08 = "#c8ccd4",
+        base0E = "#d45004", -- keywords
       },
       polish_hl = {
         treesitter = {
           ["@function.builtin"] = { bold = true },
           ["@function.call"] = { bold = true },
           ["@function.method.call"] = { bold = true },
+          -- Make all keywords the same color
+          ["@keyword"] = { fg = "#d45004" },
+          ["@keyword.function"] = { fg = "#d45004" },
+          ["@keyword.operator"] = { fg = "#d45004" },
+          ["@keyword.return"] = { fg = "#d45004" },
+          ["@keyword.repeat"] = { fg = "#d45004" },
+          ["@keyword.conditional"] = { fg = "#d45004" },
+          ["@keyword.import"] = { fg = "#d45004" },
+          ["@keyword.import.python"] = { fg = "#d45004" },
+          ["@include"] = { fg = "#d45004" },
+          ["Include"] = { fg = "#d45004" },
         },
       },
     },
