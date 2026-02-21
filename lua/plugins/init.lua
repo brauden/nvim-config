efe5426b6ff65f07aa11fc2c0b561e9159e6cc40
Dauren Bizhanov <dauren.bizhanov@duke.edu>
2026-02-20 22:11:15 -0500

    Add iron vim plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -169,4 +169,38 @@ return {
       },
     },
   },
+  {
+    "Vigemus/iron.nvim",
+    ft = { "python" },
+    config = function()
+      local iron = require "iron.core"
+      iron.setup {
+        config = {
+          -- Where the REPL opens
+          repl_open_cmd = require("iron.view").split.horizontal.botright(15),
+          repl_definition = {
+            python = {
+              command = { "ipython", "--no-autoindent" },
+              format = require("iron.fts.common").bracketed_paste,
+            },
+          },
+        },
+
+        keymaps = {
+          toggle_repl = "<space>rr",
+          send_motion = "<leader>sc",
+          visual_send = "<leader>sc",
+          send_file = "<leader>sf",
+          send_line = "<leader>sl",
+          send_until_cursor = "<leader>su",
+          cr = "<leader>s<cr>",
+          interrupt = "<leader>s<leader>",
+          exit = "<leader>sq",
+          clear = "<leader>cl",
+        },
+
+        ignore_blank_lines = true,
+      }
+    end,
+  },
 }
