7c65545a10f82e34eacd7191fbc06026db46a549
Dauren Bizhanov <dauren.bizhanov@duke.edu>
2026-03-25 18:11:45 -0400

    mixed 3 way view in diffview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -151,6 +151,13 @@ return {
   {
     "sindrets/diffview.nvim",
     lazy = false,
+    opts = {
+      view = {
+        merge_tool = {
+          layout = "diff3_mixed",
+        },
+      },
+    },
   },
   {
     "coder/claudecode.nvim",
