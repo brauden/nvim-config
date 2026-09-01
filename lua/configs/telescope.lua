94ae45d9132629a1455b5976fdbe7e339679a6fd
Dauren Bizhanov <qdauren@gmail.com>
2026-09-01 11:18:12 -0400

    Add telescope multifile open

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -1,6 +1,38 @@
 local previewers = require "telescope.previewers"
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
+
+-- <CR> opens every <Tab>-selected entry, falling back to the default action
+-- when nothing is multi-selected.
+local function open_multi(prompt_bufnr)
+  local picker = action_state.get_current_picker(prompt_bufnr)
+  local selection = picker:get_multi_selection()
+
+  if vim.tbl_isempty(selection) then
+    actions.select_default(prompt_bufnr)
+    return
+  end
+
+  actions.close(prompt_bufnr)
+
+  for _, entry in ipairs(selection) do
+    local filename = entry.path or entry.filename or entry.value
+    if type(filename) == "string" then
+      vim.cmd.edit(vim.fn.fnameescape(filename))
+      if entry.lnum then
+        pcall(vim.api.nvim_win_set_cursor, 0, { entry.lnum, (entry.col or 1) - 1 })
+      end
+    end
+  end
+end
 
 require("telescope").setup {
+  defaults = {
+    mappings = {
+      i = { ["<CR>"] = open_multi },
+      n = { ["<CR>"] = open_multi },
+    },
+  },
   pickers = {
     git_commits = {
       previewer = previewers.new_termopen_previewer {
